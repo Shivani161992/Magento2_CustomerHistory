38c94ba6889d505ebe7cec16a0871d27ad6aa305
Shivani Singh <Shivani10@mail.usf.edu>
2020-11-02 14:52:59 -0500

    added few comments

--- code/Shivani/LoginHistory/registration.php
+++ code/Shivani/LoginHistory/registration.php
@@ -1,6 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    // locate the module
     'Shivani_LoginHistory',
     __DIR__
 );
